4d0d981832a0d39091c1df08d45775507ed1f950
Shenzhennan <108182492+5456es@users.noreply.github.com>
2023-12-18 13:22:55 +0800

    Add files via upload

--- pl/WhileDU6.v
+++ pl/WhileDU6.v
@@ -9,7 +9,6 @@ Require Import SetsClass.SetsClass. Import SetsNotation.
 Require Import PL.SyntaxInCoq.
 Require Import compcert.lib.Integers.
 Require Import PL.PracticalDenotations.
-
 Local Open Scope bool.
 Local Open Scope string.
 Local Open Scope Z.
@@ -19,10 +18,10 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 
 Definition var_name: Type := string.
-
-Module Lang_WhileDU6.
 Import Lang_WhileD.
 Import Lang_While.
+Module Lang_WhileDU6.
+
 
 Inductive type : Type :=
 | TInt : type
@@ -54,6 +53,8 @@ Record state: Type := {
 
 Notation "s '.(env)'" := (env s) (at level 1).
 Notation "s '.(mem)'" := (mem s) (at level 1).
+Notation "s '.(type_env)'" := (type_env s) (at level 1).
+
 
 Record type_env_state: Type := {
     struct_info : string -> list men_var;
@@ -594,7 +595,6 @@ Definition expr_type_extract (e:expr_type): type :=
             | _ => False_sem
             end
           end
-         
         end
         with eval_l (e: expr_type): EDenote_type :=
         (*好，让我们想想能对什么东西计算左值
@@ -605,7 +605,6 @@ Definition expr_type_extract (e:expr_type): type :=
           4. EUnionMember x field ty
           5. EPoniter_Struct_Member x field ty
           6. EPoniter_Union_Member x field ty
-        
         *)
         match e with
         | EVar X ty=>
@@ -649,7 +648,7 @@ Definition expr_type_extract (e:expr_type): type :=
             {| type_nrm := ∅; type_err := Sets.full; |}
         end.
 
-
+Check state.
 
 
 
@@ -681,10 +680,10 @@ Definition SU_state_staysame (s1 s2: type_env_state): Prop :=
   s1.(type_size) = s2.(type_size).
 
 
-Notation "x '.(nrm)'" := (CDenote_type.type_nrm x)
+Notation "x '.(type_nrm)'" := (CDenote_type.type_nrm x)
   (at level 1, only printing).
 
-Notation "x '.(err)'" := (CDenote_type.type_err x)
+Notation "x '.(type_err)'" := (CDenote_type.type_err x)
   (at level 1, only printing).
 
 Ltac any_nrm x ::=
@@ -783,7 +782,7 @@ Definition asgn_deref_sem_nrm
     D2 s1 i2 /\
     s1.(mem) i1 <> None /\
     s2.(mem) i1 = Some (Vint i2) /\
-    (forall X, s1.(env) X = s2.(env) X) /\
+    (forall X, s1.(env) X = s2.(env) X /\ s1.(type_env) X = s2.(type_env) X) /\
     (forall p, i1 <> p -> s1.(mem) p = s2.(mem) p).
 
 Definition asgn_deref_sem_err
@@ -828,7 +827,22 @@ Definition declare_sem
              forall i,
                s1.(env) X <> i /\
               (*使用一块空地址*)
-              exists i', 
+              exists i', Int64.unsigned i'>=0 ->s2.(env) X = i' /\
+                  
+                (  forall k,     ( Int64.unsigned i')<=k<= (Int64.unsigned i' + (t.(type_size) ty)) -> 
+                                           ((s1.(mem) ( Int64.repr k) = Some Vuninit)  /\
+                                           (s2.(mem) ( Int64.repr k) = Some (Vint (Int64.repr 0))))
+                                           )  /\
+                (forall k': Z, k' < (Int64.unsigned i') \/ k' >= (Int64.unsigned i')+((t.(type_size) ty)) -> 
+                                                  s1.(mem) (Int64.repr k') = s2.(mem) (Int64.repr k')) /\
+            
+                (forall X', X' <> X -> s1.(env) X' = s2.(env) X' /\ s1.(type_env) X' = s2.(type_env) X')
+            ;
+                  
+                    
+
+              (*这个地址不能被占用*)
+              (*exists i', 
                 (forall pos  ipos var, (In pos (Zseq (Int64.unsigned (s1.(env) var)) (t.(type_size ) (s1.(type_env) var))))
                                         /\ (In (Int64.unsigned ipos) (Zseq (Int64.unsigned i') (t.(type_size ) (ty))))
                                         /\ s1.(env) var = ipos 
@@ -839,7 +853,9 @@ Definition declare_sem
                 (forall X', X <> X' -> s1.(env) X' = s2.(env) X') /\
                 (forall p, i' <> p -> s1.(mem) p = s2.(mem) p) /\
                 (s2.(type_env) X = ty) 
-                ;
+                ;*)
+
+
                 type_err:= fun t s =>exists i, s.(env) X = i;
                 type_inf := ∅;
   |}.
@@ -895,7 +911,624 @@ Fixpoint eval_com (c: com): CDenote_type :=
 
 End Lang_WhileDU6.
 
-Module Lang_WhileD_Malloc.
+
+Module WhileDmalloc.
+Inductive expr_malloc : Type :=
+| EConst (n: Z)  : expr_malloc
+| EVar (x: var_name) : expr_malloc
+| EBinop (op: binop) (e1 e2: expr_malloc) : expr_malloc
+| EUnop (op: unop) (e: expr_malloc) : expr_malloc
+| EDeref (e: expr_malloc) : expr_malloc
+| EAddrOf (e: expr_malloc) : expr_malloc.
+
+Record state: Type := {
+  env: var_name -> int64;
+  mem: int64 -> option val;
+}.
+
+Notation "s '.(env)'" := (env s) (at level 1).
+Notation "s '.(mem)'" := (mem s) (at level 1).
+
+Module EDenote.
+
+Record EDenote: Type := {
+  nrm: state -> int64 -> Prop;
+  err: state -> Prop;
+}.
+
+End EDenote.
+
+Import EDenote.
+
+Notation "x '.(nrm)'" := (EDenote.nrm x)
+  (at level 1, only printing).
+
+Notation "x '.(err)'" := (EDenote.err x)
+  (at level 1, only printing).
+
+Ltac any_nrm x := exact (EDenote.nrm x).
+
+Ltac any_err x := exact (EDenote.err x).
+
+Notation "x '.(nrm)'" := (ltac:(any_nrm x))
+  (at level 1, only parsing).
+
+Notation "x '.(err)'" := (ltac:(any_err x))
+  (at level 1, only parsing).
+
+  Definition arith_sem1_nrm
+  (Zfun: Z -> Z -> Z)
+  (D1 D2: state -> int64 -> Prop)
+  (s: state)
+  (i: int64): Prop :=
+exists i1 i2,
+D1 s i1 /\ D2 s i2 /\
+arith_compute1_nrm Zfun i1 i2 i.
+
+Definition arith_sem1_err
+  (Zfun: Z -> Z -> Z)
+  (D1 D2: state -> int64 -> Prop)
+  (s: state): Prop :=
+exists i1 i2,
+D1 s i1 /\ D2 s i2 /\
+arith_compute1_err Zfun i1 i2.
+
+Definition arith_sem1 Zfun (D1 D2: EDenote): EDenote :=
+{|
+nrm := arith_sem1_nrm Zfun D1.(nrm) D2.(nrm)    ;
+err := D1.(err) ∪ D2.(err) ∪
+arith_sem1_err Zfun D1.(nrm) D2.(nrm);
+|}.
+
+
+
+Definition arith_sem2_nrm
+             (int64fun: int64 -> int64 -> int64)
+             (D1 D2: state -> int64 -> Prop)
+             (s: state)
+             (i: int64): Prop :=
+  exists i1 i2,
+    D1 s i1 /\ D2 s i2 /\
+    arith_compute2_nrm int64fun i1 i2 i.
+
+Definition arith_sem2_err
+             (D1 D2: state -> int64 -> Prop)
+             (s: state): Prop :=
+  exists i1 i2,
+    D1 s i1 /\ D2 s i2 /\
+    arith_compute2_err i1 i2.
+
+Definition arith_sem2 int64fun (D1 D2: EDenote): EDenote :=
+  {|
+    nrm := arith_sem2_nrm int64fun D1.(nrm) D2.(nrm);
+    err := D1.(err) ∪ D2.(err) ∪
+           arith_sem2_err D1.(nrm) D2.(nrm);
+  |}.
+
+  Definition cmp_sem_nrm
+  (c: comparison)
+  (D1 D2: state -> int64 -> Prop)
+  (s: state)
+  (i: int64): Prop :=
+exists i1 i2,
+D1 s i1 /\ D2 s i2 /\ cmp_compute_nrm c i1 i2 i.
+
+Definition cmp_sem c (D1 D2: EDenote): EDenote :=
+{|
+nrm := cmp_sem_nrm c D1.(nrm) D2.(nrm);
+err := D1.(err) ∪ D2.(err);
+|}.
+
+Definition neg_sem_nrm
+  (D1: state -> int64 -> Prop)
+  (s: state)
+  (i: int64): Prop :=
+exists i1, D1 s i1 /\ neg_compute_nrm i1 i.
+
+
+
+Definition neg_sem_err
+             (D1: state -> int64 -> Prop)
+             (s: state): Prop :=
+  exists i1, D1 s i1 /\ neg_compute_err i1.
+
+Definition neg_sem (D1: EDenote): EDenote :=
+  {|
+    nrm := neg_sem_nrm D1.(nrm);
+    err := D1.(err) ∪ neg_sem_err D1.(nrm);
+  |}.
+
+Definition not_sem_nrm
+             (D1: state -> int64 -> Prop)
+             (s: state)
+             (i: int64): Prop :=
+  exists i1, D1 s i1 /\ not_compute_nrm i1 i.
+
+Definition not_sem (D1: EDenote): EDenote :=
+  {|
+    nrm := not_sem_nrm D1.(nrm);
+    err := D1.(err);
+  |}.
+
+  Definition and_sem_nrm
+  (D1 D2: state -> int64 -> Prop)
+  (s: state)
+  (i: int64): Prop :=
+exists i1,
+D1 s i1 /\
+(SC_and_compute_nrm i1 i \/
+NonSC_and i1 /\
+exists i2,
+D2 s i2 /\ NonSC_compute_nrm i2 i).
+Definition and_sem_err
+             (D1: state -> int64 -> Prop)
+             (D2: state -> Prop)
+             (s: state): Prop :=
+  exists i1,
+    D1 s i1 /\ NonSC_and i1 /\ D2 s.
+
+Definition and_sem (D1 D2: EDenote): EDenote :=
+  {|
+    nrm := and_sem_nrm D1.(nrm) D2.(nrm);
+    err := D1.(err) ∪ and_sem_err D1.(nrm) D2.(err);
+  |}.
+
+  
+Definition or_sem_nrm
+(D1 D2: state -> int64 -> Prop)
+(s: state)
+(i: int64): Prop :=
+exists i1,
+D1 s i1 /\
+(SC_or_compute_nrm i1 i \/
+NonSC_or i1 /\
+exists i2,
+D2 s i2 /\ NonSC_compute_nrm i2 i).
+
+Definition or_sem_err
+(D1: state -> int64 -> Prop)
+(D2: state -> Prop)
+(s: state): Prop :=
+exists i1,
+D1 s i1 /\ NonSC_or i1 /\ D2 s.
+
+Definition or_sem (D1 D2: EDenote) : EDenote :=
+{|
+nrm := or_sem_nrm D1.(nrm) D2.(nrm) ;
+err := D1.(err) ∪ or_sem_err D1.(nrm) D2.(err);
+|}.
+
+Definition unop_sem (op: unop) (D: EDenote): EDenote :=
+  match op with
+  | ONeg =>  D
+  | ONot => not_sem D
+  end.
+
+Definition binop_sem (op: binop) (D1 D2: EDenote) : EDenote :=
+  match op with
+  | OOr => or_sem D1 D2
+  | OAnd => and_sem D1 D2
+  | OLt => cmp_sem Clt D1 D2
+  | OLe => cmp_sem Cle D1 D2
+  | OGt => cmp_sem Cgt D1 D2
+  | OGe => cmp_sem Cge D1 D2
+  | OEq => cmp_sem Ceq D1 D2
+  | ONe => cmp_sem Cne D1 D2
+  | OPlus => arith_sem1 Z.add D1 D2 
+  | OMinus => arith_sem1 Z.sub D1 D2 
+  | OMul => arith_sem1 Z.mul D1 D2 
+  | ODiv => arith_sem2 Int64.divs D1 D2
+  | OMod => arith_sem2 Int64.mods D1 D2
+  end.
+
+(*这里认为常数指针和值在处理上是一样的，在使用时会因为type不同产生区别*)
+  Definition const_sem (n: Z) : EDenote :=
+    {|
+      nrm := fun s i =>
+               i = Int64.repr n /\
+               Int64.min_signed <= n <= Int64.max_signed ;
+      err := fun s =>
+               n < Int64.min_signed \/
+               n > Int64.max_signed ;
+    |}.
+  
+(** 『解引用』表达式既可以用作右值也可以用作左值。其作为右值是的语义就是原先我们
+    定义的『解引用』语义。*)
+
+Definition deref_sem_nrm
+(D1: state -> int64 -> Prop)
+(s: state)
+(i: int64): Prop :=
+exists i1, D1 s i1 /\ s.(mem)  i1 = Some (Vint i).
+
+(*这里的i1是地址*)
+
+Definition deref_sem_err
+(D1: state -> int64 -> Prop)
+(s: state): Prop :=
+exists i1,
+D1 s i1 /\
+(s.(mem) i1 = None \/ s.(mem) i1 = Some Vuninit).
+
+Definition deref_sem_r (D1: EDenote): EDenote :=
+{|
+nrm := deref_sem_nrm D1.(nrm);
+err := D1.(err) ∪ deref_sem_err D1.(nrm);
+|}.
+
+
+
+(** 当程序表达式为单个变量时，它也可以同时用作左值或右值。下面先定义其作为左值时
+的存储地址。*)
+
+Definition var_sem_l (X: var_name): EDenote :=
+{|
+nrm := fun s i => s.(env) X = i;
+err := ∅;
+|}.
+
+(*
+Definition noSU_var_sem_l (X: var_name): EDenote :=
+{|
+nrm := fun s i => s.(env) X = i /\ (s.(type_env) X) <> TStruct X /\ (s.(type_env) X) <> TUnion X;
+err := ∅;
+|}.*)
+
+(** 基于此，可以又定义它作为右值时的值。*)
+(** 基于此，可以又定义它作为右值时的值。*)
+
+
+
+Definition var_sem_r (X: var_name): EDenote :=
+  deref_sem_r (var_sem_l X).
+
+
+  (**此处，定义一个False的语句**)
+
+  Definition False_sem: EDenote :=
+    {|
+    nrm := ∅;
+    err := Sets.full;
+    |}.
+    
+    
+
+
+
+
+
+  Fixpoint eval_r (e: expr_malloc): EDenote :=
+    match e with
+    | EConst n =>
+        const_sem n
+    | EVar X =>
+        var_sem_r X
+    | EBinop op e1 e2 =>
+        binop_sem op (eval_r e1) (eval_r e2)
+    | EUnop op e1 =>
+        unop_sem op (eval_r e1)
+    | EDeref e1 =>
+        deref_sem_r (eval_r e1)
+    | EAddrOf e1 =>
+        eval_l e1
+    end
+  with eval_l (e: expr_malloc): EDenote :=
+    match e with
+    | EVar X =>
+        var_sem_l X
+    | EDeref e1 =>
+        eval_r e1
+    | _ =>
+        {| nrm := ∅; err := Sets.full; |}
+    end.
+
+
+  Definition test_true (D: EDenote):
+  state -> state -> Prop :=
+  Rels.test
+    (fun s =>
+       exists i, D.(nrm) s i /\ Int64.signed i <> 0).
+
+Definition test_false (D: EDenote):
+  state -> state -> Prop :=
+  Rels.test (fun s => D.(nrm) s (Int64.repr 0)).
+
+Module CDenote.
+
+
+Record CDenote: Type := {
+  nrm: state -> state -> Prop;
+  err: state -> Prop;
+  inf: state -> Prop
+}.
+
+End CDenote.
+Import CDenote.
+(*
+Definition SU_state_staysame (s1 s2: state): Prop :=
+
+  s1.(struct_info) = s2.(struct_info) /\
+  s1.(union_info) = s2.(union_info) /\
+  s1.(type_size) = s2.(type_size).
+*)
+
+
+
+
+Notation "x '.(nrm)'" := (CDenote.nrm x)
+  (at level 1, only printing).
+
+Notation "x '.(err)'" := (CDenote.err x)
+  (at level 1, only printing).
+
+Ltac any_nrm x ::=
+  match type of x with
+  | EDenote => exact (EDenote.nrm x)
+  | CDenote => exact (CDenote.nrm x)
+  end.
+
+Ltac any_err x ::=
+  match type of x with
+  | EDenote => exact (EDenote.err x)
+  | CDenote => exact (CDenote.err x)
+  end.
+
+Definition skip_sem: CDenote :=
+  {|
+    nrm := Rels.id;
+    err := ∅;
+    inf := ∅;
+  |}.
+
+Definition seq_sem (D1 D2: CDenote): CDenote :=
+  {|
+    nrm := D1.(nrm) ∘ D2.(nrm);
+    err := D1.(err) ∪ (D1.(nrm) ∘ D2.(err));
+    inf := D1.(inf) ∪ (D1.(nrm) ∘ D2.(inf));
+  |}.
+
+Definition if_sem
+             (D0: EDenote)
+             (D1 D2: CDenote): CDenote :=
+  {|
+    nrm := (test_true D0 ∘ D1.(nrm)) ∪
+           (test_false D0 ∘ D2.(nrm));
+    err := D0.(err) ∪
+           (test_true D0 ∘ D1.(err)) ∪
+           (test_false D0 ∘ D2.(err));
+    inf := (test_true D0 ∘ D1.(inf)) ∪
+           (test_false D0 ∘ D2.(inf))
+  |}.
+
+Fixpoint boundedLB_nrm
+           (D0: EDenote)
+           (D1: CDenote)
+           (n: nat):
+  state -> state -> Prop :=
+  match n with
+  | O => ∅
+  | S n0 =>
+      (test_true D0 ∘ D1.(nrm) ∘ boundedLB_nrm D0 D1 n0) ∪
+      (test_false D0)
+  end.
+
+Fixpoint boundedLB_err
+           (D0: EDenote)
+           (D1: CDenote)
+           (n: nat): state -> Prop :=
+  match n with
+  | O => ∅
+  | S n0 =>
+     (test_true D0 ∘
+        ((D1.(nrm) ∘ boundedLB_err D0 D1 n0) ∪
+         D1.(err))) ∪
+      D0.(err)
+  end.
+
+Definition is_inf
+             (D0: EDenote)
+             (D1: CDenote)
+             (X: state -> Prop): Prop :=
+  X ⊆ test_true D0 ∘ ((D1.(nrm) ∘ X) ∪ D1.(inf)).
+
+Definition while_sem
+             (D0: EDenote)
+             (D1: CDenote): CDenote :=
+  {|
+    nrm := ⋃ (boundedLB_nrm D0 D1);
+    err := ⋃ (boundedLB_err D0 D1);
+    inf := Sets.general_union (is_inf D0 D1);
+  |}.
+
+(** 向地址赋值的语义与原先定义基本相同，只是现在需要规定所有变量的地址不被改变，
+    而非所有变量的值不被改变。*)
+
+Definition asgn_deref_sem_nrm
+             (D1 D2: state -> int64 -> Prop)
+             (s1 s2: state): Prop :=
+  exists i1 i2,
+    D1 s1 i1 /\
+    D2 s1 i2 /\
+    s1.(mem) i1 <> None /\
+    s2.(mem) i1 = Some (Vint i2) /\
+    (forall X, s1.(env) X = s2.(env) X) /\
+    (forall p, i1 <> p -> s1.(mem) p = s2.(mem) p).
+
+Definition asgn_deref_sem_err
+             (D1: state -> int64 -> Prop)
+             (s1: state): Prop :=
+  exists i1,
+    D1 s1 i1 /\
+    s1.(mem) i1 = None.
+
+Definition asgn_deref_sem
+             (D1 D2: EDenote): CDenote :=
+  {|
+    nrm := (asgn_deref_sem_nrm D1.(nrm) D2.(nrm));
+    err := D1.(err) ∪ D2.(err) ∪
+           asgn_deref_sem_err D2.(nrm);
+    inf := ∅;
+  |}.
+
+(** 变量赋值的行为可以基于此定义。*)
+
+
+
+
+Definition asgn_var_sem
+             (X: var_name)
+             (D1: EDenote): CDenote :=
+  asgn_deref_sem (var_sem_l X) D1.
+
+Check 12.
+(* 定义 int64 序列生成函数 *)
+(*经过语法变换，在whileD+malloc中的declare语句应该传递两个参数，变量名和需要的地址数*)
+Definition declare_sem_malloc (X: var_name) (sz: Z): CDenote :=
+{|
+    nrm:= fun s1 s2 =>
+            forall i, s1.(env) X <> i /\
+            exists i': int64, (s2.(env) X = i' /\
+            ((s1.(mem) i') <> None)) /\
+            forall k: Z, 0 <= k < sz -> ((s1.(mem) (Int64.add i' (Int64.repr k)) = Some Vuninit) /\
+            (s2.(mem) (Int64.add i' (Int64.repr k)) = Some (Vint (Int64.repr 0))) /\
+            (forall k': Z, k' < (Int64.unsigned i') \/ k' >= (Int64.unsigned i')+sz -> s1.(mem) (Int64.repr k') = s2.(mem) (Int64.repr k')) /\
+            (forall X', X' <> X -> s1.(env) X' = s2.(env) X')
+            );(**)
+    err:= fun s =>exists i, s.(env) X = i;
+    inf := ∅;
+|}.
+
+Inductive com : Type :=
+  | CSkip: com
+  | CAsgnVar (x: var_name) (e: expr_malloc): com
+  | CAsgnDeref (e1 e2: expr_malloc): com
+  | CSeq (c1 c2: com): com
+  | CIf (e: expr_malloc) (c1 c2: com): com
+  | CWhile (e: expr_malloc) (c: com): com
+  | CDeclare (x: var_name) (sz: Z): com.
+
+
+Fixpoint eval_com (c: com): CDenote :=
+  match c with
+  | CSkip =>
+      skip_sem
+  | CAsgnVar X e =>
+      asgn_var_sem X (eval_r e)
+  | CAsgnDeref e1 e2 =>
+      asgn_deref_sem (eval_r e1) (eval_r e2)
+  | CSeq c1 c2 =>
+      seq_sem (eval_com c1) (eval_com c2)
+  | CIf e c1 c2 =>
+      if_sem (eval_r e) (eval_com c1) (eval_com c2)
+  | CWhile e c1 =>
+      while_sem (eval_r e) (eval_com c1)
+  | CDeclare X sz =>
+      declare_sem_malloc X sz
+  end.
+
+End WhileDmalloc.
+
+
+Import Lang_WhileDU6.
+Import WhileDmalloc.
+Import EDenote_type.
+Import CDenote_type.
+Import EDenote.
+Import CDenote.
+
+Definition state_type:=Lang_WhileDU6.state.
+Definition com_type:=Lang_WhileDU6.com.
+Notation "x '.(env')'" := ( Lang_WhileDU6.env x) (at level 1).
+Notation "x '.(mem')'" := ( Lang_WhileDU6.mem x) (at level 1).
+Notation "x '.(type_env)'" := ( Lang_WhileDU6.type_env x) (at level 1).
+
+Notation "x '.(type_nrm)'" := (EDenote_type.type_nrm x)
+  (at level 1, only printing).
+
+Notation "x '.(type_err)'" := (EDenote_type.type_err x)
+  (at level 1, only printing).
+
+Definition Lang_WhileDU6_to_WhileDmalloc_state
+           (s1: state_type)
+           (s2: state): Prop :=
+  s1.(env') = s2.(env) /\
+  s1.(mem') = s2.(mem).
+
+
+
+
+
+  (*Inductive expr_type : Type :=
+| EConst (n: Z) (ty : type) : expr_type
+| EVar (x: var_name) (ty:type): expr_type
+| EBinop (op: binop) (e1 e2: expr_type)(ty:type) : expr_type
+| EUnop (op: unop) (e: expr_type)(ty:type) : expr_type
+| EDeref (e: expr_type)(ty:type) : expr_type
+| EAddrOf (e: expr_type) (ty:type): expr_type
++
+| EStructMember (x:expr_type) (field: var_name) (ty:type): expr_type  (* Access struct member *)
+| EUnionMember (x:expr_type) (field: var_name)(ty:type) : expr_type  (* Access union member *)
+| EPoniter_Struct_Member (x:expr_type) (field: var_name)(ty:type) : expr_type  (* Access poniter member *)
+| EPoniter_Union_Member (x:expr_type) (field: var_name)(ty:type) : expr_type  (* Access poniter member *).
+
+
+| EConst (n: Z)  : expr_malloc
+| EVar (x: var_name) : expr_malloc
+| EBinop (op: binop) (e1 e2: expr_malloc) : expr_malloc
+| EUnop (op: unop) (e: expr_malloc) : expr_malloc
+| EDeref (e: expr_malloc) : expr_malloc
+| EAddrOf (e: expr_malloc) : expr_malloc.
+
+
+*)
+  Fixpoint Lang_WhileDU6_to_WhileDmalloc_Expr
+  (ty_env:Lang_WhileDU6.type_env_state)
+
+            (e1: expr_type)
+            : expr_malloc :=  
+  match e1 with
+  | Lang_WhileDU6.EConst n ty => EConst n
+  | Lang_WhileDU6.EVar X ty => EVar X
+  | Lang_WhileDU6.EBinop op e1 e2 ty => EBinop op (Lang_WhileDU6_to_WhileDmalloc_Expr ty_env e1) (Lang_WhileDU6_to_WhileDmalloc_Expr ty_env e2)
+  | Lang_WhileDU6.EUnop op e1 ty => EUnop op (Lang_WhileDU6_to_WhileDmalloc_Expr ty_env e1)
+  | Lang_WhileDU6.EDeref e1 ty => EDeref (Lang_WhileDU6_to_WhileDmalloc_Expr ty_env e1)
+  | Lang_WhileDU6.EAddrOf e1 ty => EAddrOf (Lang_WhileDU6_to_WhileDmalloc_Expr ty_env e1)
+  | Lang_WhileDU6.EStructMember e1 field ty => EAddrOf (Lang_WhileDU6_to_WhileDmalloc_Expr ty_env e1) 
+  
+  
+  
+  |_ => EConst 0
+  end.
+  
+  
+  
+  Fixpoint Lang_WhileDU6_to_WhileDmalloc_com
+            (ty_env:Lang_WhileDU6.type_env_state)
+            (c1: com_type)
+            : com :=
+  match c1 with
+  | Lang_WhileDU6.CSkip => CSkip
+  | Lang_WhileDU6.CAsgnVar X e1 => CAsgnVar X (Lang_WhileDU6_to_WhileDmalloc_Expr e1)
+  | Lang_WhileDU6.CAsgnDeref e1 e2 => CAsgnDeref (Lang_WhileDU6_to_WhileDmalloc_Expr e1) (Lang_WhileDU6_to_WhileDmalloc_Expr e2)
+  | Lang_WhileDU6.CSeq c1 c2 => CSeq (Lang_WhileDU6_to_WhileDmalloc_com  ty_env c1) (Lang_WhileDU6_to_WhileDmalloc_com  ty_env c2)
+  | Lang_WhileDU6.CIf e1 c1 c2 => CIf (Lang_WhileDU6_to_WhileDmalloc_Expr e1) (Lang_WhileDU6_to_WhileDmalloc_com ty_env c1) (Lang_WhileDU6_to_WhileDmalloc_com ty_env c2)
+  | Lang_WhileDU6.CWhile e1 c1 => CWhile (Lang_WhileDU6_to_WhileDmalloc_Expr e1) (Lang_WhileDU6_to_WhileDmalloc_com ty_env c1)
+  | Lang_WhileDU6.CDeclare X ty => CDeclare X (ty_env.(type_size) ty)
+  
+  | _ => CSkip
+  end.
+  
+            
+
+
+
+
+
+
+
+
+
+
 
 
 
